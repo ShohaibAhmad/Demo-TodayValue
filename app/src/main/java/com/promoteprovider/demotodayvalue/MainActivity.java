072757e6244a80179b6bfd1e14b11d9d56e2a39d
Shohaib Ahmad <shohaibahmad2@gmail.com>
2021-08-06 01:01:33 +0600

    sTest Navigation dr

--- app/src/main/java/com/promoteprovider/demotodayvalue/MainActivity.java
+++ app/src/main/java/com/promoteprovider/demotodayvalue/MainActivity.java
@@ -1,14 +1,24 @@
 package com.promoteprovider.demotodayvalue;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.navigation.NavigationView;
 import com.promoteprovider.demotodayvalue.Fragments.HomeFragment;
 import com.promoteprovider.demotodayvalue.Fragments.MessageFragment;
 import com.promoteprovider.demotodayvalue.Fragments.PodcastFragment;
@@ -32,10 +42,77 @@ public class MainActivity extends AppCompatActivity {
         private final static int SHORT_ID = 5;
     // start navigation Id
 
+    //DR Navigation
+    private DrawerLayout DR_Main;
+    private NavigationView DrNavigation;
+    private Toolbar toolBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //toolBar
+        toolBar = findViewById(R.id.toolBar);
+        DrNavigation = findViewById(R.id.DrNavigation);
+        DR_Main = findViewById(R.id.DR_Main);
+        //support action bar
+        setSupportActionBar(toolBar);
+        //toggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,DR_Main,toolBar,R.string.open,R.string.close);
+        DR_Main.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (DR_Main.isDrawerOpen(Gravity.RIGHT)) {
+                    DR_Main.closeDrawer(Gravity.RIGHT);
+                } else {
+                    DR_Main.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+        DrNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                DR_Main.closeDrawer(GravityCompat.END);
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.login_register:
+                        Intent intent = new Intent(MainActivity.this,Welcome.class);
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this, "Login/Register", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.video:
+                        transaction.replace(R.id.Main_Container,new VideoFragment());
+                        Toast.makeText(MainActivity.this, "Videos", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.message:
+                        transaction.replace(R.id.Main_Container,new MessageFragment());
+                        Toast.makeText(MainActivity.this, "Message", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.podcast:
+                        transaction.replace(R.id.Main_Container,new PodcastFragment());
+                        Toast.makeText(MainActivity.this, "Podcast", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.short_video:
+                        transaction.replace(R.id.Main_Container,new Short_VideoFragment());
+                        Toast.makeText(MainActivity.this, "Short Videos", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                transaction.commit();
+                return true;
+            }
+        });
+        //Change Header
+        View view = DrNavigation.getHeaderView(0);
+
         // start navigation All Code do note delete any code
         // meow find id
         meowBottomNavigation = findViewById(R.id.meowBottomNavigation);
@@ -109,5 +186,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (DR_Main.isDrawerOpen(GravityCompat.END)){
+                DR_Main.closeDrawer(GravityCompat.END);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
