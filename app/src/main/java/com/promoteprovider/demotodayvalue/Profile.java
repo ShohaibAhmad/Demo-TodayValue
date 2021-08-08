268873f7e62388c60363a6cb896ee4192c6572d3
Shohaib Ahmad <shohaibahmad2@gmail.com>
2021-08-08 23:23:37 +0600

    sTest Profile Complate

--- app/src/main/java/com/promoteprovider/demotodayvalue/Profile.java
+++ app/src/main/java/com/promoteprovider/demotodayvalue/Profile.java
@@ -2,13 +2,25 @@ package com.promoteprovider.demotodayvalue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Profile extends AppCompatActivity {
-
+    LinearLayout goToWid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        goToWid = findViewById(R.id.goToWid);
+
+        goToWid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this,withdraw.class);
+                startActivity(intent);
+            }
+        });
     }
 }
