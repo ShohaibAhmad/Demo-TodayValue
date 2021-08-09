25f40a055b049bd8579c4a9a0802dcc6491518c4
Shohaib Ahmad <shohaibahmad2@gmail.com>
2021-08-09 22:12:45 +0600

    sTest Home Page

--- app/src/main/java/com/promoteprovider/demotodayvalue/Fragments/HomeFragment.java
+++ app/src/main/java/com/promoteprovider/demotodayvalue/Fragments/HomeFragment.java
@@ -1,33 +1,47 @@
 package com.promoteprovider.demotodayvalue.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-
-import com.promoteprovider.demotodayvalue.MainActivity;
 import com.promoteprovider.demotodayvalue.R;
+import com.promoteprovider.demotodayvalue.UploadActivity;
 
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
+
+
     public HomeFragment() {
         // Required empty public constructor
     }
+
+
+    ConstraintLayout mainLayout;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mainLayout = view.findViewById(R.id.mainLayout);
 
-
-
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Intent intent = new Intent(getContext(), UploadActivity.class);
+                     startActivity(intent);
+                 }
+             });
 
         return view;
     }
-
 }
